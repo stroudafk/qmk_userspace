73b8d9eac1b15b57b9dcb95f63e4dd58e13d036b
sy <stroudafk@gmail.com>
2025-06-15 21:25:00 -0500

    Unicorne | initial commit

---
+++ keyboards/boardsource/unicorne/keymaps/sy/config.h
@@ -0,0 +1,19 @@
+#undef STARTUP_SONG
+#undef RBG_MATRIX_DEFAULT_MODE
+
+#define ENABLE_RBG_MATRIX_SOLID_COLOR
+#define RBG_MATRIX_STARTUP_MODE RBG_MATRIX_SOLID_COLOR
+#define RBG_MATRIX_DEFAULT_MODE RBG_MATRIX_SOLID_COLOR
+
+//#define COMBO_TERM 30
+#define PERMISSIVE_HOLD
+#define FLOW_TAP_TERM 150
+
+// enum layers {
+//     _COLEMAK,
+//     _QWERTY,
+//     _GAMING,
+//     _SYMBOL,
+//     _NAVIGATION,
+//     _FN
+// };
