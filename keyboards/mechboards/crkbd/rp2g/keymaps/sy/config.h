0f270b9ff2e9328e0364318c978d09cadd19e0a5
sy <stroudafk@gmail.com>
2025-06-28 10:29:40 -0500

    max | test keymap

---
+++ keyboards/mechboards/crkbd/rp2g/keymaps/sy/config.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define COMBO_TERM 30
+#define PERMISSIVE_HOLD
+#define FLOW_TAP_TERM 150
+
+// Solve early init issues. (on dock and some operating systems/distros)
+// #define SPLIT_WATCHDOG_ENABLE
+// #define SPLIT_MAX_CONNECTION_ERRORS 30
+// #define SPLIT_CONNECTION_CHECK_TIMEOUT 500
+// #define SPLIT_USB_DETECT
+// #define SPLIT_USB_TIMEOUT 1000
+// #define SPLIT_USB_TIMEOUT_POLL 20
+// #define SPLIT_WATCHDOG_TIMEOUT 5000
+
+// enum layers {
+//     _COLEMAK,
+//     _QWERTY,
+//     _GAMING,
+//     _SYMBOL,
+//     _NAVIGATION,
+//     _FN
+// };
